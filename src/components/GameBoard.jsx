793f60b34ef860b9b7d9b7523d53ec27f7017f1a
NikSyan <sarkisjanandranik@gmaill.com>
2025-07-28 11:55:45 +0200

    Add game turns between the players by lifting state up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,13 +1,38 @@
+import React, { useState } from 'react';
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  // Immutable update of the game board
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}></li>)}
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
   );
 }
